af79db5867965fea64bf40b27c1ca61d1c03675b
Peter Yoo <trustintofu@gmail.com>
2022-11-09 21:39:59 -0800

    feat: Imports header component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
+import { Header } from '../components/Header.components';
+
 export default function Home() {
   return (
     <>
+      <Header />
       <div className="">Home</div>
     </>
   );
