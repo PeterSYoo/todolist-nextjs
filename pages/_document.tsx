7cd8c359153d67b4328f676ed30e1d5e88483ae7
Peter Yoo <trustintofu@gmail.com>
2022-11-09 16:00:35 -0800

    fix: Removes title.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,7 @@ const Document = () => {
   return (
     <>
       <Html>
-        <Head>
-          <title>Todo List - NextJS</title>
-        </Head>
+        <Head></Head>
         <body className="min-h-screen min-w-screen flex flex-col">
           <div className="flex-grow">
             <Main />
