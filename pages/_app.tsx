446fe438b31172ce46046de08e6595cd4f400341
Peter Yoo <trustintofu@gmail.com>
2022-11-09 20:30:56 -0800

    feat: Wraps app components with next-themes theme provider, implements loading spinner component on page load.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,50 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { Header } from '../components/Header.components';
+import { ThemeProvider } from 'next-themes';
+import { useEffect, useState } from 'react';
+import Router from 'next/router';
+import { LoaderSpinner } from '../components/LoaderSpinner.components';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const start = () => {
+      setLoading(true);
+    };
+
+    const end = () => {
+      setLoading(false);
+    };
+
+    Router.events.on('routeChangeStart', start);
+    Router.events.on('routeChangeComplete', end);
+    Router.events.on('routeChangeError', end);
+
+    return () => {
+      Router.events.off('routeChangeStart', start);
+      Router.events.off('routeChangeComplete', end);
+      Router.events.off('routeChangeError', end);
+    };
+  }, []);
+
   return (
     <>
       <Head>
         <title>Todo List - NextJS</title>
       </Head>
-      <Header />
-      <Component {...pageProps} />
+      <ThemeProvider enableSystem={true} attribute="class">
+        {loading ? (
+          <>
+            <LoaderSpinner />
+          </>
+        ) : (
+          <>
+            <Component {...pageProps} />
+          </>
+        )}
+      </ThemeProvider>
     </>
   );
 }
