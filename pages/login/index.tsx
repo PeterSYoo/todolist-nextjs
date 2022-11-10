bc8b80f8c61fa949a5ffa73773748f437de932d1
Peter Yoo <trustintofu@gmail.com>
2022-11-09 20:31:43 -0800

    feat: Adds theme button component, adds dark mode classes to elements.

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
 import { FiLock, FiUser } from 'react-icons/fi';
+import { ThemeButton } from '../../components/ThemeButton.components';
 
 const Login = () => {
   return (
     <>
-      <section className="min-w-screen min-h-screen mx-auto flex flex-col pt-10 items-center bg-[#0B121C] md:pt-[125px] md:px-5">
+      <section className="min-w-screen min-h-screen mx-auto flex flex-col pt-10 items-center bg-[#0B121C] md:pt-[125px] md:px-5 dark:bg-gray-500">
         <div className="md:mb-7">
           {/* Mobile Logo */}
           <Image
@@ -24,7 +25,7 @@ const Login = () => {
           />
         </div>
         {/* Mobile & Desktop Login Container */}
-        <div className="mt-10 grid bg-white rounded-3xl shadow-lg shadow-black md:grid-cols-2">
+        <div className="mt-10 grid bg-white rounded-3xl shadow-lg shadow-black md:grid-cols-2 dark:bg-[#0B121C]">
           {/* Desktop Splash Image */}
           <div className="hidden md:block md:col-start-1 md:col-span-1">
             <Image
@@ -35,9 +36,12 @@ const Login = () => {
             />
           </div>
           {/* Email and Password Container */}
-          <div className="w-[228px] md:w-full mx-auto mt-6 mb-10 px-4 md:col-start-2 md:col-span-1 md:px-10 md:mt-12">
+          <div className="w-[228px] md:w-full mx-auto mt-6 mb-10 px-4 md:col-start-2 md:col-span-1 md:px-10 md:mt-6">
+            <div className="flex justify-center">
+              <ThemeButton />
+            </div>
             <div className="leading-9">Email:</div>
-            <div className="border border-gray-400 hover:border-black rounded-md px-2 flex items-center gap-2 md:py-1">
+            <div className="border border-gray-400 hover:border-black rounded-md px-2 flex items-center gap-2 md:py-1 dark:border-gray-700 hover:dark:border-white">
               <div>
                 <FiUser />
               </div>
@@ -45,12 +49,12 @@ const Login = () => {
                 <input
                   type="text"
                   placeholder="user@rapptrlabs.com"
-                  className="focus:outline-none w-full text-xs md:text-base"
+                  className="focus:outline-none w-full text-xs md:text-base dark:bg-[#0B121C]"
                 />
               </div>
             </div>
             <div className="mt-3 leading-9">Password:</div>
-            <div className="border border-gray-400 hover:border-black rounded-md px-2 flex items-center gap-2 md:py-1">
+            <div className="border border-gray-400 hover:border-black rounded-md px-2 flex items-center gap-2 md:py-1 dark:border-gray-700 hover:dark:border-white">
               <div>
                 <FiLock />
               </div>
@@ -58,12 +62,12 @@ const Login = () => {
                 <input
                   type="text"
                   placeholder="at least 4 characters"
-                  className="focus:outline-none w-full text-xs md:text-base"
+                  className="focus:outline-none w-full text-xs md:text-base dark:bg-[#0B121C]"
                 />
               </div>
             </div>
             <div className="flex justify-center mt-8 md:mt-10">
-              <button className="bg-[#0B121C] w-full rounded-lg text-white py-1 font-bold tracking-wide hover:bg-white hover:text-[#0B121C] border border-[#0B121C] hover:border hover:border-gray-400">
+              <button className="bg-[#0B121C] w-full rounded-lg text-white py-1 font-bold tracking-wide hover:bg-white hover:text-[#0B121C] border border-[#0B121C] hover:border hover:border-gray-400 dark:border-white">
                 Login
               </button>
             </div>
