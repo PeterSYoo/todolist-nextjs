c960f41acda0d8d3e598b504ca4308dc8c1f95cc
Peter Yoo <trustintofu@gmail.com>
2022-11-10 16:58:32 -0800

    feat: Removes required.

--- lib/loginValidate.ts
+++ lib/loginValidate.ts
@@ -12,17 +12,15 @@ export const loginValidate = (values: Values) => {
   const errors: Errors = {};
 
   if (!values.email) {
-    errors.email = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address';
   } else if (values.email.length > 50) {
     errors.email = 'Must not be more than 50 characters long.';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
   }
 
   if (!values.password) {
-    errors.password = 'Required';
   } else if (values.password.length < 4 || values.password.length > 16) {
-    errors.password = 'Must be greater than 4 and less than 16 characters long';
+    errors.password = 'Must be greater than 4 and less than 16 characters.';
   } else if (values.password.includes(' ')) {
     errors.password = 'Invalid Password';
   }
