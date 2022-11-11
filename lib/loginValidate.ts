85f6c216e0a87aa01a0e9ad745d472cf79de4d1f
Peter Yoo <trustintofu@gmail.com>
2022-11-10 16:16:35 -0800

    feat: Creates login validate that checks for validation errors.

---
+++ lib/loginValidate.ts
@@ -0,0 +1,31 @@
+interface Values {
+  email?: string;
+  password?: string;
+}
+
+interface Errors {
+  email?: string;
+  password?: string;
+}
+
+export const loginValidate = (values: Values) => {
+  const errors: Errors = {};
+
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  } else if (values.email.length > 50) {
+    errors.email = 'Must not be more than 50 characters long.';
+  }
+
+  if (!values.password) {
+    errors.password = 'Required';
+  } else if (values.password.length < 4 || values.password.length > 16) {
+    errors.password = 'Must be greater than 4 and less than 16 characters long';
+  } else if (values.password.includes(' ')) {
+    errors.password = 'Invalid Password';
+  }
+
+  return errors;
+};
