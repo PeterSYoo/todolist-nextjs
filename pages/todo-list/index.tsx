05d69a94c39a860158d67a881f632aaa135263d3
Peter Yoo <trustintofu@gmail.com>
2022-11-10 16:17:56 -0800

    feat: Creates todo list page.

---
+++ pages/todo-list/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from '../../components/Header.components';
+import { TodoList } from '../../components/TodoList.components';
+
+const TodoListPage = () => {
+  return (
+    <>
+      <Header />
+      <TodoList />
+    </>
+  );
+};
+
+export default TodoListPage;
