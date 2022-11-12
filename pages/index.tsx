2fe9192593efc13363f39c691b2d7bb47d3b436e
Peter Yoo <trustintofu@gmail.com>
2022-11-11 18:07:22 -0800

    feat: Home forwads user to login page if not logged in.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header.components';
 import { TodoList } from '../components/TodoList.components';
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <TodoList />
-    </>
-  );
+  const [session, setSession] = useState();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session) {
+      router.push('/login');
+    } else {
+      router.push('/todo-list');
+    }
+  }, []);
 }
