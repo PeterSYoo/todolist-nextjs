f177d8aba90bd378eb74b1ebebd20ea5fdac3ee1
Peter Yoo <trustintofu@gmail.com>
2022-11-11 18:06:02 -0800

    feat: Adds empty string error for when field is empty.

--- lib/loginValidate.ts
+++ lib/loginValidate.ts
@@ -12,6 +12,7 @@ export const loginValidate = (values: Values) => {
   const errors: Errors = {};
 
   if (!values.email) {
+    errors.email = ' ';
   } else if (values.email.length > 50) {
     errors.email = 'Must not be more than 50 characters long.';
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
@@ -19,6 +20,7 @@ export const loginValidate = (values: Values) => {
   }
 
   if (!values.password) {
+    errors.password = ' ';
   } else if (values.password.length < 4 || values.password.length > 16) {
     errors.password = 'Must be greater than 4 and less than 16 characters.';
   } else if (values.password.includes(' ')) {
